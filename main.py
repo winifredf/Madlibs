e82992d5c8e88aa04c6df7141da6db81cee235f4
Winifredf <Winifred.Dykes@hotmail.com>
2023-03-23 11:57:23 -0400

    made the Mad Lib more dynamic and less hardcoded by storing variables and getting input from the user to create varying nouns and adjectives to insert into the Mad Lib story

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-print("There once was a frog that had very itchy skin.")
-print("There was also a toad with smooth skin.")
-print("The frog was very jealous and desired to have skin like the toad.")
-print("Therefore, the frog did the unimagineable and used magic to change its skin.")
-print("Except, the magic backfired, and the frog wound up with crepy skin.")
+noun = input("Enter a noun")
+adjective = input("Enter an adjective")
+another_noun = input("Enter another noun")
+another_adjective = input("Enter another adjective")
+additional_adjective = input("Enter an additional adjective")
+
+
+print(f"There once was a {noun} that had very {adjective} skin.")
+print(f"There was also a {another_noun} with {another_adjective} skin.")
+print(f"The {noun} was very jealous and desired to have skin like the {another_noun}.")
+print(f"Therefore, the {noun} did the unimagineable and used magic to change its skin.")
+print(f"Except, the magic backfired, and the {noun} wound up with {additional_adjective} skin.")
