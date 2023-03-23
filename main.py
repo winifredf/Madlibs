2625f2993a02332eefbceb2c9986b3f9ecb47fd2
Winifredf <Winifred.Dykes@hotmail.com>
2023-03-23 12:15:58 -0400

    Inserted variables into Mad Lib story print statements

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ another_adjective = input("Enter another adjective: ")
 additional_adjective = input("Enter an additional adjective: ")
 
 
-print(f"There once was a {noun} that had very {adjective} skin.")
-print(f"There was also a {another_noun} with {another_adjective} skin.")
-print(f"The {noun} was very jealous and desired to have skin like the {another_noun}.")
-print(f"Therefore, the {noun} did the unimagineable and used magic to change its skin.")
-print(f"Except, the magic backfired, and the {noun} wound up with {additional_adjective} skin.")
+print("There once was a " + noun + " that had very " + adjective + " skin.")
+print("There was also a " + another_noun + " with " + another_adjective + " skin.")
+print("The " + noun + " was very jealous and desired to have skin like the " + another_noun + ".")
+print("Therefore, the " + noun + " did the unimagineable and used magic to change its skin.")
+print("Except, the magic backfired, and the " + noun + " wound up with " + additional_adjective + " skin.")
